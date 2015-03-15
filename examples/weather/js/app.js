a635e87a7681d6e41466390cbf5f6bb95ff9ea02
alexan <eder.alexan@gmail.com>
2015-03-15 17:25:45 +0100

    move center on small version

--- examples/weather/js/app.js
+++ examples/weather/js/app.js
@@ -59,7 +59,10 @@ moduleSystem.createModule("set-location")
 
 moduleSystem.createModule("map")
    .dependencies(["eventBus"])
-   .settings({})
+   .settings({
+      smallOffSetX: 0,
+      smallOffSetY: 100
+   })
    .creator(function (domElement, settings, eventBus) {
       var mapOptions = $.extend({
             mapTypeControl: false,
@@ -71,21 +74,76 @@ moduleSystem.createModule("map")
          }, settings.mapOptions),
          map = new google.maps.Map(domElement,
             mapOptions),
-         marker;
+         marker,
+         small = 0,
+         big = 1,
+         $window = $(window),
+         currentSize = getSize();
 
       function onLocationChanged(event) {
          clearMarker();
-         map.setCenter(event);
+         setCenter(new google.maps.LatLng(event.lat, event.lng));
          marker = new google.maps.Marker({
             position: event,
             map: map
          });
       }
 
-      $(window).on('resize', function () {
-         var currCenter = map.getCenter();
+      function setCenter(latLng) {
+         if (getSize() === small) {
+            map.panTo(latLng);
+            latLng = getProjection(latLng);
+         }
+         map.panTo(latLng);
+      }
+
+      function getProjection(latLng, revertOffset) {
+         var scale = Math.pow(2, map.getZoom()),
+            offSetX = (revertOffset) ? -settings.smallOffSetX : settings.smallOffSetX,
+            offSetY = (revertOffset) ? -settings.smallOffSetY : settings.smallOffSetY,
+            nw = new google.maps.LatLng(
+               map.getBounds().getNorthEast().lat(),
+               map.getBounds().getSouthWest().lng()
+            ),
+            worldCoordinateCenter = map.getProjection().fromLatLngToPoint(latLng),
+            pixelOffset = new google.maps.Point((offSetX / scale) || 0, (offSetY / scale) || 0),
+            worldCoordinateNewCenter = new google.maps.Point(
+               worldCoordinateCenter.x - pixelOffset.x,
+               worldCoordinateCenter.y + pixelOffset.y
+            );
+
+         return map.getProjection().fromPointToLatLng(worldCoordinateNewCenter);
+      }
+
+      function getSize() {
+         var width = $window.width();
+         if (width <= 500) {
+            return small;
+         } else {
+            return big;
+         }
+      }
+
+      $window.on('resize', function () {
+         var center = map.getCenter();
          google.maps.event.trigger(map, 'resize');
-         map.panTo(currCenter);
+
+         var size = getSize();
+         //center correction if size changes
+         if (currentSize !== size) {
+            if (currentSize === small) {
+               //change from samll to big
+               //move center down
+               center = getProjection(center, true);
+            } else {
+               //change from big to small 
+               //move center up
+               center = getProjection(center);
+            }
+            currentSize = size;
+         }
+
+         map.panTo(center);
       });
 
       google.maps.event.addListener(map, 'click', function (event) {
