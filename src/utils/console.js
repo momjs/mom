c7698eacf9b6abe677a92f1e7fa9e8ddde5598f5
iso.eder <eder.alexan@gmail.com>
2015-04-09 14:03:12 +0200

    added console polifill

---
+++ src/utils/console.js
@@ -0,0 +1,9 @@
+/* exported console */
+var console = window.console || {
+   group: function () {},
+   groupEnd: function () {},
+   log: function () {},
+   warn: function () {},
+   info: function () {},
+   error: function () {}
+};
