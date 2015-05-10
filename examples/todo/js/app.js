bab8f565e0ec0bd8e839c340c310b93014535a9d
alexan <eder.alexan@gmail.com>
2015-05-10 17:01:40 +0200

    hacky local storage

--- examples/todo/js/app.js
+++ examples/todo/js/app.js
@@ -4,47 +4,142 @@
 var removeCheckedEvent = {
       name: 'RemoveCheckedEvent',
 };
+   
+mom.createPart('uuid')
+   .creator(function() {
+   var template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+   
+   return generate;
+
+   
+   function generate() {
+      var uuid = template.replace(/[xy]/g, replaceRandom);
+      
+      return uuid;
+   }
+   
+   function replaceRandom(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+   }
+});
+   
+mom.createPart('todo-persister')
+   .creator(function() {
+   var TODO_PREFIX = 'todo';
+
+   return {
+      saveItem: saveItem,
+      deleteItem: deleteItem,
+      iterate: iterate
+   };
+   
+   function get() {
+      var todoJson = localStorage.getItem(TODO_PREFIX);
+      if(!todoJson) {
+         todoJson = '{}';
+      }
+      return JSON.parse(todoJson);
+   } 
+   
+   function getItem(todoItem) {
+      var todo = get();
+      return todo[todoItem.id];
+   }
+   
+   function save(todo) {
+      var todoJson = JSON.stringify(todo);
+      localStorage.setItem(TODO_PREFIX, todoJson);
+   }
+   
+   function saveItem(todoItem) {
+      var todo = get();
+      todo[todoItem.id] = todoItem;
+      save(todo);
+   }
+   
+   function deleteItem(id) {
+      var todo = get();
+      delete todo[id];
+      save(todo);   
+   }
+   
+   function iterate(callback) {
+      var todo = get();
+      var todoItem;
+      
+      for (var todoItemId in todo) {
+         if (todo.hasOwnProperty(todoItemId)) {
+            todoItem = todo[todoItemId];
+            callback(todoItem);
+         }
+      }
+   }
+});
 
 mom.createModule('todo-adder')
-   .creator(function(domElement) {
+   .dependencies(['uuid', 'todo-persister'])
+   .creator(function(domElement, uuid, persister) {
    var addBtn = domElement.querySelector('.js-todo-adder-add');
    var content = domElement.querySelector('.js-todo-adder-content');
-   var template='<div class="row valign-wrapper todo-item" modules="todo-item"><div class="col s10 valign"><input type="checkbox" class="filled-in js-todo-item-box" id="todo%i%"/><label for="todo%i%" class="js-todo-item-label">%text%</label></div><div class="col s2 valign"><a class="waves-effect waves-light btn red js-todo-item-remove"><i class="mdi-content-clear"></i></a></div></div>',
-       i=0;
+   var template='<div class="row valign-wrapper todo-item" modules="todo-item"><div class="col s10 valign"><input type="checkbox" class="filled-in js-todo-item-box" id="%id%" %checked%/><label for="%id%" class="js-todo-item-label">%text%</label></div><div class="col s2 valign"><a class="waves-effect waves-light btn red js-todo-item-remove"><i class="mdi-content-clear"></i></a></div></div>';
    var enterKey = 13;
-
+   
    addBtn.addEventListener('click', function() {
       addToDo();
       Materialize.updateTextFields();
       content.focus();
    });
    
-   content.onkeypress = function(e){
-      if (!e) {
+   content.onkeypress = function(e) {
+      if(!e) {
          e = window.event;
       }
       var keyCode = e.keyCode || e.which;
-      if (keyCode === enterKey){
+      if(keyCode === enterKey) {
          addToDo();
       }
    };
    
+   return {
+      postConstruct: postConstruct
+   };
+   
+   function postConstruct() {
+      setTimeout(function() {
+         persister.iterate(function(toDoItem) {
+            createToDo(toDoItem);
+         });
+      }, 0);
+   }
+
    function addToDo() {
       var text = content.value;
       var toDo;
       
       if(text !== '') {
-         toDo = createToDo(text);
-         domElement.appendChild(toDo);
+         toDo = {
+            text: text,
+            id: uuid(),
+            checked: false
+         };
+         createToDo(toDo);
+         
          content.value='';
       }
    }
    
-
-   function createToDo(text) {
-      var replacedText = template.replace('%text%', text);
-      var item = replacedText.replace(/%i%/g, i++);
-      return createDomElement(item);
+   function createToDo(toDo) {
+      var toDoHtml = template;
+      var toDoElement;
+      
+      toDoHtml = toDoHtml.replace('%text%', toDo.text)
+      toDoHtml = toDoHtml.replace(/%id%/g, toDo.id);
+      toDoHtml = toDoHtml.replace('%checked%', (toDo.checked) ? 'checked' : '');
+      
+      toDoElement = createDomElement(toDoHtml);
+      
+      domElement.appendChild(toDoElement);
    }
 
    function createDomElement(string) {
@@ -56,30 +151,40 @@ mom.createModule('todo-adder')
 
 
 mom.createModule('todo-item')
-   .creator(function(domElement) {
+   .dependencies(['todo-persister'])
+   .creator(function(domElement, persister) {
    var checkbox = domElement.querySelector('.js-todo-item-box');
-   var checked = checkbox.checked;
    var label = domElement.querySelector('.js-todo-item-label');
    var remove = domElement.querySelector('.js-todo-item-remove');
+   var item = {
+      id: checkbox.id,
+      text: label.innerText,
+      checked: checkbox.checked
+   };
    
    render();
    
+   persister.saveItem(item);
+      
    remove.addEventListener('click', function() {
       removeItem();
    });
    
+   
    checkbox.addEventListener('change', function() {
-      checked = checkbox.checked;
+      item.checked = checkbox.checked;
       render();
+      persister.saveItem(item);
    });
    
    function removeItem() {
       domElement.parentElement.removeChild(domElement);
+      persister.deleteItem(item.id);
    }
    
 
    function render() {
-      if(checked) {
+      if(item.checked) {
          label.classList.add('todo-item--checked');
       } else {
          label.classList.remove('todo-item--checked');
@@ -87,7 +192,7 @@ mom.createModule('todo-item')
    }
    
    function onRemoveChecked() {
-      if(checked) {
+      if(item.checked) {
          removeItem();
       }
    }
